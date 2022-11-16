7af0241a315da2d6b66f1e9ef892b267ae6a93cc
Kimdonghun <zhfptm12@naver.com>
2022-11-16 19:41:30 +0900

    feat : server

--- server.py
+++ server.py
@@ -2,3 +2,16 @@ from flask import Flask
 
 
 print('hello')
+
+
+app=Flask(__name__)
+@app.route('/users')
+def users():
+	# users 데이터를 Json 형식으로 반환한다
+    return {"members": [{ "id" : 1, "name" : "yerin" },
+    					{ "id" : 2, "name" : "dalkong" }]}
+           
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    
