9d7ef52dddbe940c14e37dbdeffafc654f11fe37
Kimdonghun <zhfptm12@naver.com>
2022-11-17 02:28:43 +0900

    Fix : 프록시 url수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,9 @@
+//src/setupProxy.js
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "api",
+    "/api",
     createProxyMiddleware({
       target: "http://127.0.0.1:5000",
       changeOrigin: true,
