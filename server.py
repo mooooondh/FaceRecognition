1db354538eefebe62969dedfc17d5d21e462b098
Kimdonghun <zhfptm12@naver.com>
2022-11-17 02:29:14 +0900

    Refactor : api url 수정

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ print('hello')
 
 
 app=Flask(__name__)
-@app.route('/users')
+@app.route('/api/users')
 def users():
 	# users 데이터를 Json 형식으로 반환한다
     return {"members": [{ "id" : 1, "name" : "yerin" },
