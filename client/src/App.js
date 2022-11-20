7e3eed5b9adab8dc1007a276470a10f5bb9fa463
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2022-11-20 12:59:35 +0900

    [fix] React bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import { useState, useEffect } from "react";
 function App() {
   // state
   const [data, setData] = useState([{}]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("/api/users")
-      .then(response => response.json())
-      .then(data => {
+  const getData = async () => {
+    await fetch("/api/users")
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         // 받아온 데이터를 data 변수에 update
         setData(data);
       })
-      .catch(err => console.log(`err : ${err}`));
+      .catch((err) => console.log(`err : ${err}`));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
 
   return (
@@ -20,11 +26,11 @@ function App() {
       <h1>test 하는 중...</h1>
       <div>
         {/* 삼항연산자 */}
-        {typeof data.users === "undefined" ? (
+        {loading ? (
           // fetch가 완료되지 않았을 경우에 대한 처리
           <p>loding...</p>
         ) : (
-          data.users.map(u => <p>{u.name}</p>)
+          data.members.map((u) => <p>{u.name}</p>)
         )}
       </div>
     </div>
