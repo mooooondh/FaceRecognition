aae6bc74a325d608facb911ad9176cb81630f9c4
Kimdonghun <zhfptm12@naver.com>
2022-12-04 02:24:06 +0900

    ASD

--- server.py
+++ server.py
@@ -38,38 +38,38 @@ def image():
 def learn_model() :
     target_img=0
     model = load_model('girl_boy_predict_epoch40.h5')
-    print("model", model)
+    # print("model", model)
     a = np.array([0,1,2,3,4])
     print(a)
     target = os.listdir("./웹이미지")
-    for file in target:
-        img_data = load_img("./웹이미지/" + file)
-        print(img_data)
-        # img_data = Image.open("./웹이미지/" + file)
-        # img_data.show()
-        # img_black = img_data.convert("L")
-        # img_black.show()
-        # img = img_black.resize((48
-        # 0,480))
-        img_arr= img_to_array(img_data)/255
-        print(img_arr.size)
+    # for file in target:
+    #     img_data = load_img("./웹이미지/" + file)
+    #     print(img_data)
+    #     # img_data = Image.open("./웹이미지/" + file)
+    #     # img_data.show()
+    #     # img_black = img_data.convert("L")
+    #     # img_black.show()
+    #     # img = img_black.resize((48
+    #     # 0,480))
+    #     img_arr= img_to_array(img_data)/255
+    #     print(img_arr.size)
 
-        img= img_arr.reshape((1,)+ img_arr.shape)
-        print(img.size)
-        target_img = img
+    #     img= img_arr.reshape((1,)+ img_arr.shape)
+    #     print(img.size)
+    #     target_img = img
 
-        # target_img = np.vstack(target_img,img)
-        # img= img_arr.reshape((1,)+ img_arr.shape)
+    #     # target_img = np.vstack(target_img,img)
+    #     # img= img_arr.reshape((1,)+ img_arr.shape)
         
-        print(file)
-    y_test_what= np.array([0]* 1) 
-    y_test_1= np.array([1]* 1)
-    y_test_what= np.append(y_test_what, y_test_1)
+    #     print(file)
+    # y_test_what= np.array([0]* 1) 
+    # y_test_1= np.array([1]* 1)
+    # y_test_what= np.append(y_test_what, y_test_1)
 
-    Y_test = model.predict(target_img)
-    Y_test1 = Y_test.argmax(axis=-1)
+    # Y_test = model.predict(target_img)
+    # Y_test1 = Y_test.argmax(axis=-1)
     
-    print(Y_test1)
+    # print(Y_test1)
 
 if __name__ == "__main__":
     app.run(debug = True)
