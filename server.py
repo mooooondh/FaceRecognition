7025fbd2f11c40340b391a648d2664d3015fea83
Kimdonghun <zhfptm12@naver.com>
2022-12-04 13:39:19 +0900

    image

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 from keras.models import load_model
 from tensorflow.keras.utils import *
 from tensorflow.image import *
-
+from flask_cors import CORS
 from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,6 +14,7 @@ print('hello')
 
 
 app=Flask(__name__)
+cors = CORS(app, resources={"/api/*": {"origins": "http://localhost:3000"}})
 @app.route('/api/users')
 def users():
 	# users 데이터를 Json 형식으로 반환한다
@@ -24,10 +25,11 @@ def users():
 @app.route('/api/image', methods = ['POST', "GET"])
 def image():
     
-    learn_model()
+    # learn_model()
     if request.method == 'POST':
         f = request.files['file']
         # return f.filename
+        print(f)
         f.save('./webimage/'+ secure_filename(f.filename))
         return 'file 이 저장되었습니다.'
     
@@ -43,21 +45,25 @@ def learn_model() :
     print(a)
     target = os.listdir("./webimage")
     for file in target:
+        print("file : " + file)
         img_data = load_img("./webimage/" + file)
         print(img_data)
-        # img_data = Image.open("./웹이미지/" + file)
+        img_data = Image.open("./webimage/" + file)
         # img_data.show()
-        # img_black = img_data.convert("L")
+        img_black = img_data.convert("L")
         # img_black.show()
         img = img_black.resize((480,480))
+        # img.show()
+        
         img_arr= img_to_array(img_data)/255
-        print(img_arr.size)
+        # print(img_arr.size)
 
-    #     img= img_arr.reshape((1,)+ img_arr.shape)
-    #     print(img.size)
-    #     target_img = img
+        img= img_arr.reshape((1,)+ img_arr.shape)
+        print(img.size)
+        print('변환 완료')
+        target_img = img
 
-    #     # target_img = np.vstack(target_img,img)
+        # target_img = np.vstack(target_img,img)
     #     # img= img_arr.reshape((1,)+ img_arr.shape)
         
     #     print(file)
@@ -65,10 +71,10 @@ def learn_model() :
     # y_test_1= np.array([1]* 1)
     # y_test_what= np.append(y_test_what, y_test_1)
 
-    # Y_test = model.predict(target_img)
-    # Y_test1 = Y_test.argmax(axis=-1)
+    Y_test = model.predict(target_img)
+    Y_test1 = Y_test.argmax(axis=-1)
     
-    # print(Y_test1)
+    print(Y_test1)
 
 if __name__ == "__main__":
     app.run(debug = True)
